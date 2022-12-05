a273aebf56782f1b11f5ef6ec52bbcab1ac706bb
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-06 02:07:08 +0300

    Update 0-privileges.sql

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,4 +1,3 @@
--- lists all privileges
--- users are user_0d_1 and user_0d_2
-SHOW GRANTS FOR 'user_0d_1'@'localhost';
-SHOW GRANTS FOR 'user_0d_2'@'localhost';
+-- lists all privileges of MySQL users
+SHOW GRANTS FOR user_0d_1@localhost;
+SHOW GRANTS FOR user_0d_2@localhost;
