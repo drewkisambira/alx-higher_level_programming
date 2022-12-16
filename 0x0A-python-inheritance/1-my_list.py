b0eb518ed7d5a2bdb1b33760cef2f35f56153e8e
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-16 06:16:06 -0800

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,1 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+inherits from class list
+and prints a sorted list
+"""
 
+
+class MyList(list):
+    """
+    inherits from list
+    methods:
+        print_sorted(self)
+    """
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
