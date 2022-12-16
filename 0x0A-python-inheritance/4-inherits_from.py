cc0410ee2c3035e51cd069742d768bf78d9dff5c
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-16 06:21:47 -0800

    task 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,1 +1,15 @@
+#!/usr/bin/python3
+"""
+Function that returns true if object is
+an instance of a class that inherited
+frm a specified class
+"""
 
+
+def inherits_from(obj, a_class):
+    """
+    function defination with arguments as
+    obj and a_class
+    """
+
+    return isinstance(obj, a_class) and type(obj) != a_class
