1ceacf61168f74561039f9dbfce1a0c4499716ec
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-20 07:23:42 -0800

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,21 @@
 #!/usr/bin/node
 
-if (process.argv.length === 2) {
+function func1() {}
+function func2(a) {}
+function func3(a, b) 
+console.log(func1.length);
+console.log(func2.length);
+console.log(func3.length);
+if (func1){
   console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else {
-  console.log('Arguments found');
 }
+else if (func2)
+   console.log('Argument found');
+}
+else {
+  console.log('Argument found');
+}
+
+
 
 
