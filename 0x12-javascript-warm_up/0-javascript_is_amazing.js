c06cff092aca9e700ebe751b04f8b674d746ffa0
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-20 06:40:14 -0800

    task 0

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
 //javascript code that prints from avariable
-myVar = 'JavaScript is amazing';
-console.log(const myVar)
+const myVar = 'JavaScript is amazing';
+console.log(myVar);
