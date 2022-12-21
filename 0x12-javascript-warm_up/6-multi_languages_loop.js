601fac879cb00a790b9b1794630416a9605e1647
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-21 06:09:47 -0800

    task 6

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const Languages = ['C is fun', 'Python is cool', 'Javascript is amazing'];
-for ( let i = 0; , i < Languages.length; , i++){
-  console.log(langu[i]);
+for (let i = 0; i < Languages.length; i++) {
+  console.log(Languages[i]);
 }
