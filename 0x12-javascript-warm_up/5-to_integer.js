a50da097676d16b8e0372af08b32e2a106420ecd
Kisambira Andrew <andrewkeshaa@gmail.com>
2022-12-21 05:53:02 -0800

    task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
-const myNum = parseInt(process.argv[2], 10);
-if (isNaN(myNum)) {
+/*
+script that prints My number: <first argument converted in integer> if the first argument can be converted to an integer
+*/
+const strtoint = parseInt(process.argv[2]);
+if (isNaN(strtoint)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ' + myNum);
+  console.log('My number: ' + strtoint);
 }
-
-
